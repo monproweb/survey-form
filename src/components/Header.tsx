521a5ee0b7b06990f817d67a9c8ffe49d8f7e700
Thomas Erhel <thomas.erhel@gmail.com>
2021-04-17 03:14:17 +0200

    Add code splitting

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="header">
+            <h1 id="title" className="text-center">Mon Pro Web Survey Form</h1>
+            <p id="description" className="description text-center">Thank you for taking the time to help us improve the platform</p>
+        </header>
+    );
+}
+
+export default Header;
